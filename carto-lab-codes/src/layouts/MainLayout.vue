4679abf9b622fc28a511efdc41905ee8244cd8a3
ywyue <865051699@qq.com>
2021-06-12 10:23:06 +0200

    fix menu bouncing issue

--- carto-lab-codes/src/layouts/MainLayout.vue
+++ carto-lab-codes/src/layouts/MainLayout.vue
@@ -2,12 +2,24 @@
   <div>
     <q-layout view="hhh LpR fff">
       <q-header elevated height="200">
-        <q-toolbar style="background-color:white">
-          <q-btn flat dense round icon="menu" aria-label="Menu" class="q-mr-sm" size="0.8em" @click="leftDrawerOpen = !leftDrawerOpen" />
+        <q-toolbar style="background-color: white">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            class="q-mr-sm"
+            size="0.8em"
+            @click="leftDrawerOpen = !leftDrawerOpen"
+          />
           <q-toolbar-title>
             <div class="items-center">
               <q-icon class="text-dark q-ma-md-lg" size="70px">
-                <img src="images/globalization.svg" style="width:50px;height:50px"/>
+                <img
+                  src="images/globalization.svg"
+                  style="width: 50px; height: 50px"
+                />
               </q-icon>
               <strong>TITLE HERE</strong>
             </div>
@@ -18,19 +30,37 @@
 
       <!-- Left menu -->
       <!-- on mouseover shows name-->
-      <q-drawer v-model="leftDrawerOpen" show-if-above :mini="miniState" @mouseover="miniState = false" @mouseout="miniState = true" mini-to-overlay :width="350" :breakpoint="500" content-class="bg-grey-3" @on-layout="leftDrawerToggled">
-        <q-scroll-area class="fit">
-          <q-list padding>
-            <Menu :menuItems="menuItems" />
-          </q-list>
-        </q-scroll-area>
+      <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :mini="miniState"
+        @mouseover="miniState = false"
+        @mouseout="miniState = true"
+        mini-to-overlay
+        :width="350"
+        :breakpoint="500"
+        content-class="bg-grey-3"
+        @on-layout="leftDrawerToggled"
+      >
+        <q-list padding>
+          <Menu :menuItems="menuItems" />
+        </q-list>
       </q-drawer>
 
       <q-page-container style="height: 100vh">
         <Index />
         <!-- place QPageScroller at end of page -->
-        <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]" >
-          <q-btn fab icon="keyboard_arrow_up" background="rgba(39, 155, 72, 0.5)" style="z-index:100"/>
+        <q-page-scroller
+          position="bottom-right"
+          :scroll-offset="150"
+          :offset="[18, 18]"
+        >
+          <q-btn
+            fab
+            icon="keyboard_arrow_up"
+            background="rgba(39, 155, 72, 0.5)"
+            style="z-index: 100"
+          />
         </q-page-scroller>
       </q-page-container>
     </q-layout>
@@ -38,35 +68,35 @@
 </template>
 
 <script>
-import Menu from 'layouts/Menu.vue';
-import Index from 'pages/Index';
+import Menu from "layouts/Menu.vue";
+import Index from "pages/Index";
 
 const menuItems = [
   {
-    title: 'Introduction',
-    icon: 'favorite',
+    title: "Introduction",
+    icon: "favorite",
   },
   {
-    title: 'Global situation',
-    icon: 'fact_check',
+    title: "Global situation",
+    icon: "fact_check",
   },
   {
-    title: 'China',
-    icon: 'track_changes',
+    title: "China",
+    icon: "track_changes",
   },
   {
-    title: 'Policies and measurements',
-    icon: 'public',
+    title: "Policies and measurements",
+    icon: "public",
   },
   {
-    title: 'Conclusion',
-    icon: 'insights',
-  }
+    title: "Conclusion",
+    icon: "insights",
+  },
 ];
 
 export default {
-  name: 'MainLayout',
-  components: { Menu, Index},
+  name: "MainLayout",
+  components: { Menu, Index },
   data() {
     return {
       leftDrawerOpen: false,
@@ -82,5 +112,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- carto-lab-codes/src/layouts/Menu.vue
+++ carto-lab-codes/src/layouts/Menu.vue
@@ -1,32 +1,50 @@
 <template>
-<!--  Menu on the left of the page -->
-  <div v-for="(item, index) in menuItems" :key="item.title">
-    <q-item clickable v-ripple @click="setActiveIndex(index)" :class="activeIndex === index ? 'active' : ''">
-      <q-item-section v-if="item.icon" avatar><q-icon :name="item.icon" /></q-item-section>
-      <q-item-section>
-        <q-item-label>{{ item.title }}</q-item-label>
-        <q-item-label caption> {{ item.caption }}</q-item-label>
-      </q-item-section>
-    </q-item>
-    <div v-if="index != menuItems.length - 1" style="horizontal-align: center; text-align: center; color: rgba(0,0,0,0)">|</div>
+  <!--  Menu on the left of the page -->
+  <div>
+    <div v-for="(item, index) in menuItems" :key="item.title">
+      <q-item
+        clickable
+        v-ripple
+        @click="setActiveIndex(index)"
+        :class="activeIndex === index ? 'active' : ''"
+      >
+        <q-item-section v-if="item.icon" avatar
+          ><q-icon :name="item.icon"
+        /></q-item-section>
+        <q-item-section>
+          <q-item-label>{{ item.title }}</q-item-label>
+          <q-item-label caption> {{ item.caption }}</q-item-label>
+        </q-item-section>
+      </q-item>
+      <div
+        v-if="index != menuItems.length - 1"
+        style="
+          horizontal-align: center;
+          text-align: center;
+          color: rgba(0, 0, 0, 0);
+        "
+      >
+        |
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Menu',
-  props: ['menuItems'],
+  name: "Menu",
+  props: ["menuItems"],
   data() {
     return {};
   },
   methods: {
     setActiveIndex(e) {
       // set index to current viewing page
-      this.$store.commit('sider/updateIndex', e);
-      let c = document.getElementById('scroll').children;
+      this.$store.commit("sider/updateIndex", e);
+      let c = document.getElementById("scroll").children;
       c.forEach((v, i) => {
-        if (v.getAttribute('s') == e) {
-          c[i].scrollIntoView({ behavior: 'smooth' });
+        if (v.getAttribute("s") == e) {
+          c[i].scrollIntoView({ behavior: "smooth" });
         }
       });
     },
@@ -40,6 +58,6 @@ export default {
 </script>
 <style scoped>
 .active {
-  background-color: #279B48;
+  background-color: #279b48;
 }
 </style>
