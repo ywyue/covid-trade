74249ba089fcf6d38b92089872cb6e9281e05c17
ywyue <865051699@qq.com>
2021-06-17 17:46:27 +0200

    update policy

--- carto-lab-codes/src/components/contents/ChinaSituation.vue
+++ carto-lab-codes/src/components/contents/ChinaSituation.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page>
     <session-banner
-      title="China ...."
-      subtitle="subtitle....."
       img-url="images/container.jpg"
+      title="China Trade During COVID-19"
+      subtitle="The health crisis has unleashed a global economic shock"
     />
+    <subsession-title title="COVID-19 Impacts the Global Economy" />
     <div
       class="flourish-embed flourish-globe"
       data-src="visualisation/6440972"
@@ -22,9 +23,17 @@
 import ChinaTradeTreemap from "components/charts/chinaTradeTreemap";
 import TestSunburst from "components/charts/testSunburst";
 import tradeCartogram from "components/maps/tradeCartogram";
+import SessionBanner from "components/tools/SessionBanner";
+import SubsessionTitle from "components/tools/SubsessionTitle";
 export default {
   name: "ChinaSituation",
-  components: { TestSunburst, ChinaTradeTreemap, tradeCartogram },
+  components: {
+    SubsessionTitle,
+    SessionBanner,
+    TestSunburst,
+    ChinaTradeTreemap,
+    tradeCartogram,
+  },
   mounted() {
     let recaptchaScript = document.createElement("script");
     recaptchaScript.setAttribute(
