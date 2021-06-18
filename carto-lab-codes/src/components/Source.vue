4d58193988bf2a0e4185f746aeb38d848f0bb110
ywyue <865051699@qq.com>
2021-06-18 23:18:01 +0200

    add China trade text

--- carto-lab-codes/src/components/Source.vue
+++ carto-lab-codes/src/components/Source.vue
@@ -1,34 +1,61 @@
 <template>
-  <div class="q-pa-md items-center" style="background-color:#e0e0e0; height: 60px; margin-top:0px; display:flex; justify-content:center;">
-    <q-btn @click="scroll" flat align="around"
-           label="Source"
-           icon="lightbulb_outline"
-           style="margin:auto; display:block;"/>
+  <div
+    class="q-pa-md items-center"
+    style="
+      background-color: #e0e0e0;
+      height: 60px;
+      margin-top: 0px;
+      display: flex;
+      justify-content: center;
+    "
+  >
+    <q-btn
+      @click="scroll"
+      flat
+      align="around"
+      label="Source"
+      icon="source"
+      style="margin: auto; display: block"
+    />
   </div>
 
-  <div class="row" style="background-color:#e0e0e0;" v-if="show" id="source">
+  <div class="row" style="background-color: #e0e0e0" v-if="show" id="source">
     <div class="column">
-      <span style="color:#505050; font-size:120%">Data Source:</span>
-      <div class="text-body2"><a href="https://www.imf.org/external/pubs/ft/weo/disclaim.htm">IMF World Economic Outlook Database</a></div>
-      <div class="text-body2"><a href="https://unctadstat.unctad.org/wds/ReportFolders/reportFolders.aspx">UNCTAD STAT</a></div>
-      <div class="text-body2"><a href="https://marketplace.officialstatistics.org/ais-weekly-port-calls">AIS: Weekly Port Calls</a></div>
+      <span style="color: #505050; font-size: 120%">Data Source:</span>
+      <div class="text-body2">
+        <a href="https://www.imf.org/external/pubs/ft/weo/disclaim.htm"
+          >IMF World Economic Outlook Database</a
+        >
+      </div>
+      <div class="text-body2">
+        <a
+          href="https://unctadstat.unctad.org/wds/ReportFolders/reportFolders.aspx"
+          >UNCTAD STAT</a
+        >
+      </div>
+      <div class="text-body2">
+        <a
+          href="https://marketplace.officialstatistics.org/ais-weekly-port-calls"
+          >AIS: Weekly Port Calls</a
+        >
+      </div>
     </div>
-    <q-separator vertical inset color="white"/>
+    <q-separator vertical inset color="white" />
     <div class="column">
-      <span style="color:#505050; font-size:120%">Software:</span>
+      <span style="color: #505050; font-size: 120%">Software:</span>
     </div>
-    <q-separator vertical inset color="white"/>
+    <q-separator vertical inset color="white" />
     <div class="column">
-      <span style="color:#505050; font-size:120%">Images: </span>
-      <div class="text-body2"><a href="https://www.flaticon.com/">FLATICON.COM</a></div>
+      <span style="color: #505050; font-size: 120%">Images: </span>
+      <div class="text-body2">
+        <a href="https://www.flaticon.com/">FLATICON.COM</a>
+      </div>
     </div>
-    <q-space class="q-ma-md"/>
+    <q-space class="q-ma-md" />
   </div>
   <div class="text-body2 q-pl-lg q-pr-lg q-pb-sm">
     &copy; 2021 Yuanwen Yue & Jingyan Li. All rights reserved.
   </div>
-
-
 </template>
 
 <script>
@@ -36,59 +63,59 @@ export default {
   name: "Source",
   data() {
     return {
-      show: false
-    }
+      show: false,
+    };
   },
   methods: {
     scroll() {
       if (this.show == true) {
-        this.show = false
-      }else{
-        this.show = true
+        this.show = false;
+      } else {
+        this.show = true;
         setTimeout(() => {
-          const element = document.getElementById('source');
-          element.scrollIntoView({ behavior: 'smooth' });
-        }, 100)
+          const element = document.getElementById("source");
+          element.scrollIntoView({ behavior: "smooth" });
+        }, 100);
       }
-    }
+    },
   },
-}
+};
 </script>
 
-<style  scoped>
+<style scoped>
+.column {
+  float: left;
+  width: 33%;
+  padding-left: 2%;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.no_underline {
+  text-decoration: none;
+}
+
+p {
+  font-size: 90%;
+}
+
+a {
+  color: #505050;
+  text-decoration: underline;
+  font-size: 80%;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+@media screen and (max-width: 600px) {
   .column {
-    float: left;
-    width: 33%;
-    padding-left: 2%;
+    width: 100%;
   }
-
-  .row:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-
-  .no_underline {
-    text-decoration: none;
-  }
-
-  p {
-    font-size: 90%;
-  }
-
-  a {
-    color: #505050;
-    text-decoration: underline;
-    font-size: 80%;
-  }
-
-  html {
-    scroll-behavior: smooth;
-  }
-
-  @media screen and (max-width: 600px) {
-    .column {
-      width: 100%;
-    }
-  }
+}
 </style>
--- carto-lab-codes/src/components/contents/ChinaSituation.vue
+++ carto-lab-codes/src/components/contents/ChinaSituation.vue
@@ -2,15 +2,48 @@
   <q-page>
     <session-banner
       img-url="images/container3.jpg"
-      title="China Trade During COVID-19"
+      title="China's Trade During COVID-19"
       subtitle="The health crisis has unleashed a global economic shock"
     />
-    <subsession-title title="COVID-19 Impacts the Global Economy" />
+    <subsession-title title="Visualization of China's trade flow" />
+
+    <div class="row justify-center">
+      <div class="col-12 col-md-8">
+        <div class="text-body1 q-mt-lg q-mb-lg">
+          Since 2014, China is not only the world's largest exporter, but also
+          the largest trading nation in terms of the sum of its exports and
+          imports. According to UN Comtrade Database, China exported $259.06
+          billion in goods in 2020 while it imported $205.559 billion. Despite
+          the impact of COVID, China’s exports have increased by 3.68% in 2020
+
+          <q-space class="q-ma-md" />
+          The U.S. has been the TOP 1 export destination of Chinese goods for
+          the past 5 years. Regarding imports, most of China's imports come from
+          South Korea, Japan and the United States.
+        </div>
+      </div>
+    </div>
+
     <div
       class="flourish-embed flourish-globe"
-      data-src="visualisation/6440972"
-      data-url="https://flo.uri.sh/visualisation/6440972/embed"
+      data-src="visualisation/6468975"
+      data-url="https://flo.uri.sh/visualisation/6468975/embed"
     ></div>
+
+    <div class="row justify-center">
+      <div class="col-12 col-md-8">
+        <div class="text-body1 q-mt-lg q-mb-lg">
+          Detailed information about the types of commodities traded of China in
+          2020 can be found from the tree map below. The most recent imports of
+          China are led by Crude Petroleum (26.19%), Iron Ore (17.67%), and
+          Petroleum Gas (6.25%).
+
+          <q-space class="q-ma-md" />
+          The most recent exports are led by
+        </div>
+      </div>
+    </div>
+
     <div>
       <trade-commodity-destination />
     </div>
