ab7fe5aab5efb91c892a75c18df7ae1ba5286938
JingyanLI <37017983+JenneaLa@users.noreply.github.com>
2021-06-18 12:11:08 +0200

    add timeline in intro

---
+++ carto-lab-codes/src/components/contents/Intro/TimelineCovid.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="q-px-lg q-pb-md">
+    <q-timeline :layout="layout" color="secondary">
+<!--      <q-timeline-entry heading>-->
+<!--        Key dates of COVID-19-->
+<!--      </q-timeline-entry>-->
+
+      <q-timeline-entry
+        title="January 9, 2020"
+        subtitle="January 9, 2020"
+        side="left"
+      >
+        <div>
+          WHO Announces Mysterious Coronavirus-Related Pneumonia in Wuhan, China
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="January 21, 2020"
+        subtitle="January 21, 2020"
+        side="right"
+      >
+        <div>
+          Chinese Scientist Confirms COVID-19 Human Transmission
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="January 31, 2020"
+        subtitle="January 31, 2020"
+        side="left"
+      >
+        <div>
+          WHO Issues Global Health Emergency
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="February 2, 2020"
+        subtitle="February 2, 2020"
+        side="right"
+      >
+        <div>
+          Global Air Travel Is Restricted
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="March 11, 2020"
+        subtitle="March 11, 2020"
+        side="left"
+      >
+        <div>
+          WHO Declares COVID-19 a Pandemic
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="March 15, 2020"
+        subtitle="March 15, 2020"
+        side="right"
+      >
+        <div>
+          Reports Find Extended Shutdowns Can Delay Second Wave
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+  </div>
+</template>
+
+<script>
+export default {
+name: "TimelineCovid",
+  computed: {
+    layout () {
+      return this.$q.screen.lt.sm ? 'dense' : (this.$q.screen.lt.md ? 'comfortable' : 'loose')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- carto-lab-codes/src/components/contents/Introduction.vue
+++ carto-lab-codes/src/components/contents/Introduction.vue
@@ -1,9 +1,26 @@
 <template>
   <q-page>
     <div class="text-center text-h2">Introduction</div>
+    <q-space class="q-ma-xl"/>
+    <div>
+      <timeline-covid/>
+    </div>
+    <q-space class="q-ma-xl"/>
+    <div class="text-center items-center row justify-center">
+      <div class="col-12 col-md-8">
+        <div class="text-body1">
+          Since the outbreak of the coronavirus disease of 2019 (COVID-19), more than 1 million people
+          have lost their lives due to the pandemic, and the global economy is expected to contract
+          by a staggering 4.3% in 2020. Millions of jobs have already been lost, millions of
+          livelihoods are at risk, and an estimated additional 130 million people will be living in extreme
+          poverty if the crisis persists.
+        </div>
+      </div>
+    </div>
+    <q-space class="q-ma-xl"/>
     <!--    Key Numbers   -->
-    <div class="row items-start text-center" v-intersection="startAnim">
-      <div class="col-12 col-md-4">
+    <div class="row items-start text-center justify-center" v-intersection="startAnim">
+      <div class="col-12 col-md-3">
         <number-counter
           number="1"
           unit="million+"
@@ -12,7 +29,7 @@
           ref="c1"
         />
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-md-3">
         <number-counter
           number="4.3"
           unit="percent"
@@ -21,7 +38,7 @@
           ref="c2"
         />
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-md-3">
         <number-counter
           number="130"
           unit="million"
@@ -32,17 +49,6 @@
       </div>
     </div>
     <q-space class="q-ma-xl" />
-    <div class="text-body1">
-      Major date:
-      <q-space/>
-      January 9 — WHO Announces Mysterious Coronavirus-Related Pneumonia in Wuhan, China
-      January 21 — Chinese Scientist Confirms COVID-19 Human Transmission
-      January 31 — WHO Issues Global Health Emergency
-      February 2 — Global Air Travel Is Restricted
-      March 11 — WHO Declares COVID-19 a Pandemic
-      March 25 — Reports Find Extended Shutdowns Can Delay Second Wave
-    </div>
-    <q-space class="q-ma-xl"/>
     <GlobeLayout/>
   </q-page>
 </template>
@@ -50,11 +56,12 @@
 <script>
 import NumberCounter from "components/tools/NumberCounter";
 import GlobeLayout from "components/contents/Intro/GlobeLayout";
+import TimelineCovid from "components/contents/Intro/TimelineCovid";
 
 
 export default {
   name: "Introduction",
-  components: {GlobeLayout, NumberCounter },
+  components: {TimelineCovid, GlobeLayout, NumberCounter },
   data: function () {
     return {};
   },
